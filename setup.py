98090733e5b903f80c40654f5f2791b49d77cb65
Pratha <prathamesh.pawar2610@gmail.com>
2019-10-24 21:09:48 -0700

    Initial commit

--- setup.py
+++ setup.py
@@ -14,18 +14,15 @@ def readme():
 setup(
     name='NYC',
     version='0.1',
-    description='Predicting taxi trip duration from one location to another',
+    description='Predicting NYC taxi trip duration from one location to the other',
     long_description=readme(),
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
-	install_requires=[
-      	'pypandoc>=1.4'
-    ]
     # Substitute <github_account> with the name of your GitHub account
     url='https://github.com/pratha19/NYC_taxi_trip_duration',
     author='Prathamesh Pawar',  # Substitute your name
     author_email='pawar.pratha@gmail.com',  # Substitute your email
-    license= 'None',
+    license='MIT',
     packages=['NYC'],
 )
