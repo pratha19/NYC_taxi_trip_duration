1f501fc2d872757427c0d86ac597780f21b3e7fb
Pratha <prathamesh.pawar2610@gmail.com>
2019-10-24 20:39:54 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+def readme():
+    """Import the README.md Markdown file and try to convert it to RST format."""
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        with open('README.md') as readme_file:
+            return readme_file.read()
+
+
+setup(
+    name='NYC',
+    version='0.1',
+    description='Predicting taxi trip duration from one location to another',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+	install_requires=[
+      	'pypandoc>=1.4'
+    ]
+    # Substitute <github_account> with the name of your GitHub account
+    url='https://github.com/pratha19/NYC_taxi_trip_duration',
+    author='Prathamesh Pawar',  # Substitute your name
+    author_email='pawar.pratha@gmail.com',  # Substitute your email
+    license= 'None',
+    packages=['NYC'],
+)
